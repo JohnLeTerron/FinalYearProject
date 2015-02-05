cf65a61e42c0c3b803ec324c9cf582a9b6868dde
William <williamlea@Williams-iMac.local>
2015-02-05 18:35:29 +0000

    Added some transform variables to SceneObject

--- mobile/src/main/java/com/leterronapps/hyperfour/game/SceneObject.java
+++ mobile/src/main/java/com/leterronapps/hyperfour/game/SceneObject.java
@@ -1,12 +1,20 @@
 package com.leterronapps.hyperfour.game;
 
+import com.leterronapps.hyperfour.util.Vector3D;
+
 /**
  * Created by williamlea on 05/02/15.
  */
 public abstract class SceneObject {
 
-    public SceneObject() {
+    public Vector3D position;
+    public Vector3D rotation;
+    public Vector3D scale;
 
+    public SceneObject() {
+        position = new Vector3D();
+        rotation = new Vector3D();
+        scale = new Vector3D(1.0f, 1.0f, 1.0f);
     }
 
     public abstract void update(float deltaTime);
